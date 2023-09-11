754e1a2ddee7154843239001478a91df9e898fab
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-09-11 16:14:49 +0530

    Added delete functionality

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import api from "./api/api";
+import ConfirmationDialog from "./components/ConfirmationDialog";
 
 interface Transaction {
   // Define the type for a transaction item
@@ -10,6 +11,7 @@ interface Transaction {
   description: string;
   is_income: boolean;
   date: string;
+  onDelete: () => void;
 }
 
 function App() {
@@ -21,6 +23,11 @@ function App() {
     is_income: false,
     date: "",
   });
+
+  const [confirmationVisible, setConfirmationVisible] = useState(false);
+  const [transactionToDelete, setTransactionToDelete] = useState<number | null>(
+    null
+  );
 
   const fetchTransaction = async () => {
     try {
@@ -47,9 +54,7 @@ function App() {
     });
   };
 
-  const handleFormSubmit = async (
-    event: FormEvent<HTMLFormElement>
-  ) => {
+  const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     await api.post("/addtransaction/", formData);
     fetchTransaction();
@@ -60,6 +65,33 @@ function App() {
       is_income: false,
       date: "",
     });
+  };
+
+  const handleDeleteTransaction = (transactionId: number) => {
+    setConfirmationVisible(true);
+    setTransactionToDelete(transactionId);
+  };
+
+  const confirmDelete = async () => {
+    if (transactionToDelete !== null) {
+      try {
+        await api.delete(`/deletetransaction/${transactionToDelete}`);
+        fetchTransaction();
+      } catch (error) {
+        console.error(
+          `Error deleting transaction ${transactionToDelete}:`,
+          error
+        );
+      } finally {
+        setConfirmationVisible(false);
+        setTransactionToDelete(null);
+      }
+    }
+  };
+
+  const cancelDelete = () => {
+    setConfirmationVisible(false);
+    setTransactionToDelete(null);
   };
 
   return (
@@ -125,70 +157,99 @@ function App() {
                 className="w-full p-3 mt-2"
               />
             </div>
-            <button type="submit" className="w-full px-2 py-3 bg-blue-500 text-center mt-4">
+            <button
+              type="submit"
+              className="w-full px-2 py-3 bg-blue-500 text-center mt-4"
+            >
               Submit
             </button>
           </form>
           <table className="min-w-full mt-10 divide-y divide-gray-700 dark:bg-gray-800">
-  <thead className="bg-gray-600 dark:bg-gray-700">
-    <tr>
-      <th
-        scope="col"
-        className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-      >
-        Amount
-      </th>
-      <th
-        scope="col"
-        className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-      >
-        Category
-      </th>
-      <th
-        scope="col"
-        className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-      >
-        Description
-      </th>
-      <th
-        scope="col"
-        className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-      >
-        Income
-      </th>
-      <th
-        scope="col"
-        className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
-      >
-        Date
-      </th>
-    </tr>
-  </thead>
-  <tbody className="bg-gray-700 dark:bg-gray-800 divide-y divide-gray-600 dark:divide-gray-700">
-    {transactions.map((tran) => (
-      <tr key={tran.id}>
-        <td className="px-6 py-4 whitespace-nowrap text-gray-200 dark:text-white">{tran.amount}</td>
-        <td className="px-6 py-4 whitespace-nowrap text-gray-200 dark:text-white">{tran.category}</td>
-        <td className="px-6 py-4 whitespace-nowrap text-gray-200 dark:text-white">{tran.description}</td>
-        <td className="px-6 py-4 whitespace-nowrap text-gray-200 dark:text-white">
-          {tran.is_income ? (
-            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-500 dark:bg-green-300 text-green-100 dark:text-green-800">
-              Yes
-            </span>
-          ) : (
-            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-500 dark:bg-red-300 text-red-100 dark:text-red-800">
-              No
-            </span>
-          )}
-        </td>
-        <td className="px-6 py-4 whitespace-nowrap text-gray-200 dark:text-white">{tran.date}</td>
-      </tr>
-    ))}
-  </tbody>
-</table>
-
+            <thead className="bg-gray-600 dark:bg-gray-700">
+              <tr>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                >
+                  Amount
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                >
+                  Category
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                >
+                  Description
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                >
+                  Income
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                >
+                  Date
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
+                >
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-gray-700 dark:bg-gray-800 divide-y divide-gray-600 dark:divide-gray-700">
+              {transactions.map((tran) => (
+                <tr key={tran.id}>
+                  <td className="px-6 py-4 whitespace-nowrap text-gray-200 dark:text-white">
+                    {tran.amount}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-gray-200 dark:text-white">
+                    {tran.category}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-gray-200 dark:text-white">
+                    {tran.description}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-gray-200 dark:text-white">
+                    {tran.is_income ? (
+                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-500 dark:bg-green-300 text-green-100 dark:text-green-800">
+                        Yes
+                      </span>
+                    ) : (
+                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-500 dark:bg-red-300 text-red-100 dark:text-red-800">
+                        No
+                      </span>
+                    )}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-gray-200 dark:text-white">
+                    {tran.date}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
+                    <button
+                      onClick={() => handleDeleteTransaction(tran.id)}
+                      className="text-red-600 hover:text-red-900"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
+      <ConfirmationDialog
+        visible={confirmationVisible}
+        onConfirm={confirmDelete}
+        onCancel={cancelDelete}
+      />
     </>
   );
 }
