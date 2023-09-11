ba89ff22ff46b5b38d77494f44ba8f5aa696e4c9
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-09-11 22:45:52 +0530

    Corrected the baseUrl of api

--- React/src/api/api.ts
+++ React/src/api/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-    // baseURL: "https://fastapi-react-app-backend.onrender.com/",
-    baseURL: 'http://127.0.0.1:8000/',
+    baseURL: "https://fastapi-react-app-backend.onrender.com/",
+    // baseURL: 'http://127.0.0.1:8000/',
 });
 
 export default api;
